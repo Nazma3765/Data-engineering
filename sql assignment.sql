e4c977ed62bcd65b136760d98a3805910b868121
Nazma3765 <nazma3765@gmail.com>
2025-03-11 09:58:54 +0530

    Add files via upload

---
+++ sql assignment.sql
@@ -0,0 +1,65 @@
+--   select *from  SH.SALES
+-- select sysdate from sh.sales
+-- select systimestamp from sh.sales
+-- select dbtimezone from sh.sales
+-- select sysdate,sysdate+7 from sh.sales
+-- select sysdate,sysdate-17 from sh.sales
+-- select time_id,time_id+7 from sh.sales
+-- select systimestamp,systimestamp+interval '2' hour from sh.sales
+--  select systimestamp,systimestamp+interval '2' minute from sh.sales
+--  select systimestamp,systimestamp+interval '2' second from sh.sales
+--  select systimestamp,systimestamp+interval '2' month from sh.sales
+--  select systimestamp,systimestamp+interval '2' year from sh.sales
+--  select systimestamp,systimestamp+interval '2'day+interval '4'hour from sh.sales
+-- select TIME_ID,extract (year from TIME_ID)year,extract (month from time_id)month,extract (day from time_id)day from sh.sales
+-- select time_id,last_day(time_id)from sh.sales
+ 
+
+
+--  select *from sh.sales
+--   SELECT  prod_id FROM sh.sales
+--  SELECT *  FROM sh.sales  WHERE quantity_sold > 5
+-- select *from sh.sales where prod_id =(P1001)
+select *from sh.sales where prod_id =(40)
+select *from prod_id count(*) sh.sales
+
+
+
+
+
+SELECT * 
+FROM sh.sales
+WHERE time_id= sysdate - INTERVAL '30' DAY;
+##Extract the year from the sales date column.
+select time_id,extract(year from time_id)year from sh.sales
+##Extract the month from the sales date column. 
+select time_id,extract(month from time_id)month from sh.sales
+SELECT * 
+FROM sh.sales
+WHERE EXTRACT(DOW FROM time_id) IN (0, 6);
+SELECT * 
+FROM sh.sales
+WHERE TO_CHAR(time_id, 'D') IN ('1', '7');
+select *from sh.sales
+##Find the first and last recorded sales transaction dates.
+ SELECT 
+--     MIN(time_id) AS first_time_id,
+--     MAX(time_id) AS last_time_id
+-- FROM sh.sales;
+##Count the number of sales transactions that happened in the last 7 days.
+SELECT COUNT(*) AS count
+FROM sh.sales
+WHERE time_id = SYSDATE - 7;
+
+##Retrieve sales transactions that happened on a specific date ('2024-03-01')
+SELECT * 
+FROM sh.sales
+WHERE time_id = '3/1/2024';  ---invalid month 
+
+SELECT * 
+FROM sh.sales
+WHERE time_id = TO_DATE('01-MAR-2024', 'DD-MON-YYYY');
+
+SELECT * 
+FROM sh.sales
+WHERE TIME(time_id)) BETWEEN '09:00:00' AND '11:59:59';
